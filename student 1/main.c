8f593fe333942fd1c42a65eb1275d079454a9c36
michealsafwat <michealsafwat310@gmail.com>
2021-07-10 21:39:02 +0200

    student 1

---
+++ student 1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//Team Members : Khaled Khalifa - Mostafa Gamal - Micheal Safwat
+
+struct student
+{
+
+    char name[50];
+    int ID;
+    int db[3];
+    int score;
+};
+
+
+int main()
+{
+    printf("********WELCOME********\n");
+
+    return 0;
+}
